10135a73ffa6459b0592f506259bf5a533d99bcb
Violeta Georgieva <696661+violetagg@users.noreply.github.com>
2026-06-08 09:38:34 +0300

    Refine header handling during redirects
    Signed-off-by: Violeta Georgieva <696661+violetagg@users.noreply.github.com>

--- reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClientConnect.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClientConnect.java
@@ -575,7 +575,7 @@ class HttpClientConnect extends HttpClient {
 				}
 
 				Consumer<HttpClientRequest> consumer = null;
-				if (fromURI != null && !toURI.equals(fromURI)) {
+				if (fromURI != null && (!toURI.equals(fromURI) || (fromURI.isSecure() && !toURI.isSecure()))) {
 					if (handler instanceof RedirectSendHandler) {
 						headers.remove(HttpHeaderNames.EXPECT)
 						       .remove(HttpHeaderNames.COOKIE)
